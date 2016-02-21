f05d2f90e1e9fd75dbea7a41dccf74e3069139e1
NylsSs <nilsredoulez@gmail.com>
2016-02-21 23:21:00 +0100

    fonction js pour calculer la hauteur du premier bloc de la homepage

--- app/homepage/homepage.controller.js
+++ app/homepage/homepage.controller.js
@@ -8,3 +8,23 @@
     vm.title = 'Homepage';
   }
 })();
+
+$(document).ready(function(){
+	heightElem();
+	heightElemResize();
+});
+
+var heightElem = function(){
+	var windowHeight = $(window).outerHeight();
+	var headerHeight = $('#header').outerHeight();
+	var restHeight = windowHeight - headerHeight;
+	$('.home-top').outerHeight(restHeight);
+};
+
+var heightElemResize = function(){
+	$(window).resize(function(){
+		setTimeout(function(){ 
+			heightElem();
+		}, 800);
+	});
+};
